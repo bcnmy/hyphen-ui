5ff579320af035072483914c90387fd6286073de
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-20 11:52:02 +0530

    Added a GraphQL provider for querying Graph.

--- src/pages/home/components/UserInfoModal/index.tsx
+++ src/pages/home/components/UserInfoModal/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 import { Dialog } from "@headlessui/react";
 import { useWalletProvider } from "context/WalletProvider";
 import { IoMdClose } from "react-icons/io";
@@ -8,17 +9,44 @@ import {
 } from "react-icons/hi";
 import Modal from "../../../../components/Modal";
 import { getProviderInfo } from "web3modal";
+import { useChains } from "context/Chains";
 
 export interface IUserInfoModalProps {
   isVisible: boolean;
   onClose: () => void;
 }
 
+const USER_TRANSACTIONS = gql`
+  query USER_TRANSACTIONS($address: String!) {
+    fundsDepositeds(
+      first: 5
+      orderBy: timestamp
+      orderDirection: desc
+      where: { from: $address }
+    ) {
+      id
+      from
+      receiver
+      tokenAddress
+      amount
+      toChainId
+      timestamp
+    }
+  }
+`;
+
 function UserInfoModal({ isVisible, onClose }: IUserInfoModalProps) {
   const [addressCopied, setAddressCopied] = useState(false);
   const { accounts, rawEthereumProvider } = useWalletProvider()!;
+  const { fromChain } = useChains()!;
   const userAddress = accounts?.[0];
+  const { loading, error, data } = useQuery(USER_TRANSACTIONS, {
+    skip: !isVisible,
+    variables: { address: userAddress },
+  });
   const { name: providerName } = getProviderInfo(rawEthereumProvider);
+
+  console.log({ loading, error, data });
 
   function handleUserAddressCopy() {
     navigator.clipboard.writeText(userAddress || "");
@@ -41,7 +69,7 @@ function UserInfoModal({ isVisible, onClose }: IUserInfoModalProps) {
         </div>
 
         <article className="p-4 mx-6 mb-6 border border-gray-200 rounded-2xl">
-          <header className="flex items-center justify-between mb-2">
+          <header className="flex items-center justify-between mb-3">
             <p className="text-sm text-gray-500">
               Connected with {providerName}
             </p>
@@ -52,10 +80,7 @@ function UserInfoModal({ isVisible, onClose }: IUserInfoModalProps) {
           <p className="mb-2 text-2xl text-gray-700">
             {userAddress?.slice(0, 6)}...{userAddress?.slice(-6)}
           </p>
-          <button
-            className="flex items-center px-2 py-1 -mx-2 rounded hover:bg-gray-100"
-            onClick={handleUserAddressCopy}
-          >
+          <button className="flex items-center" onClick={handleUserAddressCopy}>
             {addressCopied ? (
               <HiOutlineClipboardCheck className="w-auto h-4 mr-1 text-green-400" />
             ) : (
@@ -73,7 +98,7 @@ function UserInfoModal({ isVisible, onClose }: IUserInfoModalProps) {
 
         <article className="px-6 py-4 bg-gray-100 rounded-bl-3xl rounded-br-3xl">
           <span className="text-lg text-gray-500">
-            Recent transactions will appear here...
+            {loading ? "Loading..." : "Recent Transactions"}
           </span>
         </article>
       </div>

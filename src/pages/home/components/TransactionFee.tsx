9ee381850625f71bd817a2524d23555d3b9353e3
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-25 18:12:11 +0530

    Added tooltips to tx costs details.

--- src/pages/home/components/TransactionFee.tsx
+++ src/pages/home/components/TransactionFee.tsx
@@ -1,11 +1,13 @@
 import { useTransaction } from "context/Transaction";
 import * as React from "react";
-import { FaInfoCircle } from "react-icons/fa";
 import { Status } from "hooks/useLoading";
 import { Transition } from "react-transition-group";
 import { twMerge } from "tailwind-merge";
 import Skeleton from "react-loading-skeleton";
 import { useToken } from "context/Token";
+import { HiInformationCircle } from "react-icons/hi";
+import CustomTooltip from "./CustomTooltip";
+import { useChains } from "context/Chains";
 
 interface ITransactionFeeProps {}
 
@@ -18,6 +20,7 @@ const TransactionFee: React.FunctionComponent<ITransactionFeeProps> = (
     transactionAmountValidationErrors,
   } = useTransaction()!;
   const { selectedToken } = useToken()!;
+  const { toChain } = useChains()!;
 
   return (
     <Transition
@@ -35,12 +38,18 @@ const TransactionFee: React.FunctionComponent<ITransactionFeeProps> = (
             (state === "exiting" || state === "exited") && "-translate-y-full"
           )}
         >
-          <div className="relative z-0 mx-10 bg-white border-b shadow-lg bg-opacity-10 backdrop-blur border-white/10 border-x rounded-b-md">
-            <div className="absolute opacity-80 -inset-[2px] bg-gradient-to-br from-white/10 to-hyphen-purple/30 blur-md -z-10"></div>
+          <div className="mx-10 bg-white border-b bg-opacity-10 border-white/10 border-x rounded-b-md">
             <div className="grid grid-cols-2 p-6 text-sm text-white/75 gap-y-2">
-              <span className="flex items-center gap-2 font-medium">
-                <FaInfoCircle /> Liquidity Provider Fee
-              </span>
+              <div className="flex items-center gap-2 font-medium">
+                <HiInformationCircle data-tip data-for="lpFee" />
+                {transactionFee ? (
+                  <CustomTooltip
+                    id="lpFee"
+                    text={`${transactionFee.lpFeeProcessedString}% fee to be given to liquidity providers`}
+                  />
+                ) : null}
+                Liquidity Provider Fee
+              </div>
               <span className="font-mono text-right">
                 {fetchTransactionFeeStatus === Status.SUCCESS &&
                 transactionFee ? (
@@ -54,7 +63,13 @@ const TransactionFee: React.FunctionComponent<ITransactionFeeProps> = (
                 )}
               </span>
               <span className="flex items-center gap-2 font-medium">
-                <FaInfoCircle />
+                <HiInformationCircle data-tip data-for="transactionFee" />
+                {toChain ? (
+                  <CustomTooltip
+                    id="transactionFee"
+                    text={`Fee corresponding to the transaction done by Biconomy to transfer funds on ${toChain.name}. It varies as per the market gas price on ${toChain.name}.`}
+                  />
+                ) : null}
                 Transaction Fee
               </span>
               <span className="font-mono text-right">
@@ -70,7 +85,13 @@ const TransactionFee: React.FunctionComponent<ITransactionFeeProps> = (
                 )}
               </span>
               <span className="flex items-center gap-2 font-medium">
-                <FaInfoCircle />
+                <HiInformationCircle data-tip data-for="minimumFunds" />
+                {toChain ? (
+                  <CustomTooltip
+                    id="minimumFunds"
+                    text={`Minimum funds you will get on ${toChain.name}. Actual amount may vary slightly based on on-chain data.`}
+                  />
+                ) : null}
                 You get minimum
               </span>
               <span className="font-mono text-right">

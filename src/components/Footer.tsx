0a64a58df23c32e6d39630bdb841d6429235670c
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-28 17:40:49 +0530

    Added link to biconomy in footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ import poweredByBiconomy from "../assets/images/powered-by-biconomy.svg";
 function Footer() {
   return (
     <footer className="fixed bottom-0 flex items-center w-full h-12 px-6 py-4">
-      <img src={poweredByBiconomy} alt="Powered by Biconomy" />
+      <a target="_blank" href="https://biconomy.io/" rel="noreferrer">
+        <img src={poweredByBiconomy} alt="Powered by Biconomy" />
+      </a>
     </footer>
   );
 }

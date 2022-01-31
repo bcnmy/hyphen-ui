935fa917041e8fa3313d399ef4627a8407665f2d
Asheer Rizvi <asheer.vil@gmail.com>
2022-01-31 10:45:08 +0530

    Disables the manual exit button after the first click.

--- src/components/Buttons/PrimaryButtonDark.tsx
+++ src/components/Buttons/PrimaryButtonDark.tsx
@@ -15,7 +15,7 @@ export const PrimaryButtonDark: React.FC<IPrimaryButtonDarkProps> = ({
   return (
     <button
       className={twMerge(
-        "px-4 py-2 bg-white bg-opacity-10 shadow-md rounded-full text-white text-opacity-75 border border-opacity-10 border-white ml-auto font-normal relative backdrop-blur-md cursor-pointer hover:bg-opacity-[0.15] hover:text-opacity-90 hover:border-opacity-20 hover:shadow-lg transition",
+        "px-4 py-2 bg-white bg-opacity-10 shadow-md rounded-full text-white text-opacity-75 border border-opacity-10 border-white ml-auto font-normal relative backdrop-blur-md cursor-pointer hover:bg-opacity-[0.15] hover:text-opacity-90 hover:border-opacity-20 hover:shadow-lg transition disabled:cursor-not-allowed",
         className
       )}
       {...props}

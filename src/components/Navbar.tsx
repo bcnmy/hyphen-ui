e0484aa9771bcc1f4f4862b8f672dddef85a874e
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-11 15:32:39 +0530

    Added support links to footer.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,31 +10,34 @@ function Navbar({ showUserInfoModal }: INavbarProps) {
   const userAddress = accounts?.[0];
 
   return (
-    <div className="flex items-center justify-between w-full px-6 text-white border-b border-black shadow-sm bg-hyphen-purple bg-opacity-10 backdrop-blur-sm border-opacity-20">
-      <div className="flex">
+    <header className="bg-hyphen-purple relative flex w-full items-center justify-center border-b border-black border-opacity-20 bg-opacity-10 text-white shadow-sm backdrop-blur-sm">
+      <a href="/" className="absolute left-6">
         <img
           src={`${process.env.PUBLIC_URL}/hyphen-logo.svg`}
-          className="w-auto h-8 mr-8"
+          className="h-8 w-auto"
           alt="Hyphen Logo"
         />
+      </a>
+      <nav className="flex gap-7 text-white">
         <a
           target="_blank"
           href="https://hyphen-info.biconomy.io/"
           rel="noreferrer"
-          className="flex items-center text-white"
+          className="relative flex items-center text-white"
         >
-          Analytics <HiOutlineArrowSmRight className="w-5 h-5 -rotate-45" />
+          Stats
+          <HiOutlineArrowSmRight className="absolute top-[2px] right-[-12px] h-3 w-3 -rotate-45" />
         </a>
-      </div>
+      </nav>
       <button
-        className="px-4 py-1 bg-white bg-opacity-10 rounded-xl text-white font-mono border border-opacity-10 border-white font-base relative backdrop-blur-md cursor-pointer hover:bg-opacity-[0.15] hover:text-opacity-90 hover:border-opacity-20"
+        className="font-base absolute right-6 cursor-pointer rounded-xl border border-white border-opacity-10 bg-white bg-opacity-10 px-4 py-1 font-mono text-white backdrop-blur-md hover:border-opacity-20 hover:bg-opacity-[0.15] hover:text-opacity-90"
         onClick={isLoggedIn ? showUserInfoModal : connect}
       >
         {isLoggedIn
           ? `${userAddress?.slice(0, 6)}...${userAddress?.slice(-6)}`
           : "Connect Wallet"}
       </button>
-    </div>
+    </header>
   );
 }
 

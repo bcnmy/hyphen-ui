e15b9ddf57c06d850ef64e752f1b4314dc337c94
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-15 14:14:51 +0530

    Added a network selector to the header and completed it's UI.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Link, NavLink } from "react-router-dom";
-import { useWalletProvider } from "context/WalletProvider";
-import { HiOutlineArrowSmRight } from "react-icons/hi";
+import { Link, NavLink } from 'react-router-dom';
+import { useWalletProvider } from 'context/WalletProvider';
+import { HiOutlineArrowSmRight } from 'react-icons/hi';
+import NetworkSelector from './NetworkSelector';
 
 interface INavbarProps {
   showUserInfoModal: () => void;
@@ -11,30 +12,30 @@ function Navbar({ showUserInfoModal }: INavbarProps) {
   const userAddress = accounts?.[0];
 
   return (
-    <header className="relative flex items-center justify-center w-full text-white border-b border-black shadow-sm bg-hyphen-purple bg-opacity-10 backdrop-blur-sm border-opacity-20">
+    <header className="relative z-20 flex w-full items-center justify-center bg-black bg-opacity-30 text-white shadow-sm backdrop-blur-sm">
       <Link to="/bridge" className="absolute left-6">
         <img
           src={`${process.env.PUBLIC_URL}/hyphen-logo.svg`}
-          className="w-auto h-8"
+          className="h-8 w-auto"
           alt="Hyphen Logo"
         />
       </Link>
-      <nav className="flex text-white gap-7">
+      <nav className="flex gap-7 text-white">
         <NavLink to="/bridge">
           {({ isActive }) => (
             <span
               className={
                 isActive
-                  ? "relative"
-                  : "relative text-gray-400 hover:text-white"
+                  ? 'relative'
+                  : 'relative text-gray-400 hover:text-white'
               }
             >
               Bridge
               <span
                 className={
                   isActive
-                    ? "absolute block h-[5px] bg-white -inset-1 top-[33px] rounded-t-full"
-                    : "hidden"
+                    ? 'absolute -inset-1 top-[33px] block h-[5px] rounded-t-full bg-white'
+                    : 'hidden'
                 }
                 aria-hidden="true"
               ></span>
@@ -43,22 +44,22 @@ function Navbar({ showUserInfoModal }: INavbarProps) {
         </NavLink>
         <NavLink
           to="/pool"
-          className={({ isActive }) => (isActive ? "relative" : "relative")}
+          className={({ isActive }) => (isActive ? 'relative' : 'relative')}
         >
           {({ isActive }) => (
             <span
               className={
                 isActive
-                  ? "relative"
-                  : "relative text-gray-400 hover:text-white"
+                  ? 'relative'
+                  : 'relative text-gray-400 hover:text-white'
               }
             >
               Pool
               <span
                 className={
                   isActive
-                    ? "absolute block h-[5px] bg-white -inset-1 top-[33px] rounded-t-full"
-                    : "hidden"
+                    ? 'absolute -inset-1 top-[33px] block h-[5px] rounded-t-full bg-white'
+                    : 'hidden'
                 }
                 aria-hidden="true"
               ></span>
@@ -72,17 +73,20 @@ function Navbar({ showUserInfoModal }: INavbarProps) {
           className="relative flex items-center text-gray-400 hover:text-white"
         >
           Stats
-          <HiOutlineArrowSmRight className="absolute w-3 h-3 -rotate-45 top-[2px] right-[-12px]" />
+          <HiOutlineArrowSmRight className="absolute top-[2px] right-[-12px] h-3 w-3 -rotate-45" />
         </a>
       </nav>
-      <button
-        className="absolute right-6 px-4 py-1 bg-white bg-opacity-10 rounded-xl text-white font-mono border border-opacity-10 border-white font-base backdrop-blur-md cursor-pointer hover:bg-opacity-[0.15] hover:text-opacity-90 hover:border-opacity-20"
-        onClick={isLoggedIn ? showUserInfoModal : connect}
-      >
-        {isLoggedIn
-          ? `${userAddress?.slice(0, 6)}...${userAddress?.slice(-6)}`
-          : "Connect Wallet"}
-      </button>
+      <div className="absolute right-6 flex items-center">
+        <NetworkSelector />
+        <button
+          className="font-base ml-2.5 cursor-pointer rounded-xl bg-hyphen-purple bg-opacity-50 px-4 py-1 font-mono text-white"
+          onClick={isLoggedIn ? showUserInfoModal : connect}
+        >
+          {isLoggedIn
+            ? `${userAddress?.slice(0, 6)}...${userAddress?.slice(-6)}`
+            : 'Connect Wallet'}
+        </button>
+      </div>
     </header>
   );
 }
---
+++ src/components/NetworkSelector.tsx
@@ -0,0 +1,75 @@
+import { Fragment, useState } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { HiOutlineChevronDown } from 'react-icons/hi';
+import { useChains } from 'context/Chains';
+
+function NetworkSelector() {
+  const { chainsList } = useChains()!;
+  const networks = chainsList.map((chain) => ({
+    id: chain.chainId,
+    name: chain.name,
+    image: chain.image,
+  }));
+
+  const [selected, setSelected] = useState(networks[0]);
+
+  return (
+    <div className="w-[146px]">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative">
+          <Listbox.Button className="relative h-8 w-full cursor-pointer rounded-xl bg-hyphen-purple bg-opacity-50 pl-3 pr-10 text-left focus:outline-none sm:text-sm">
+            <span className="flex items-center truncate">
+              <img
+                src={selected.image}
+                alt={selected.name}
+                className="mr-2 h-5 w-5"
+              />
+              {selected.name}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <HiOutlineChevronDown
+                className="h-4 w-4 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-xl bg-hyphen-purple py-1 text-base focus:outline-none sm:text-sm">
+              {networks.map((network) => (
+                <Listbox.Option
+                  key={network.id}
+                  className="relative flex cursor-pointer select-none items-center py-2 pl-3 pr-4 text-white hover:bg-hyphen-purple-dark hover:bg-opacity-50"
+                  value={network}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <img
+                        src={network.image}
+                        alt={network.name}
+                        className="mr-2 h-5 w-5"
+                      />
+                      <span
+                        className={`${
+                          selected ? 'font-medium' : 'font-normal'
+                        } block truncate`}
+                      >
+                        {network.name}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
+
+export default NetworkSelector;

8599349f1b0b795c024510b30a268e85bda2292a
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-16 14:15:42 +0530

    Added React Query for fetching smart contract data, used skeleton components in YourPositions components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Outlet } from 'react-router-dom';
 import Layout from '../src/components/Layout';
 
+// Refetch the data after 1 hour.
+const oneHourInMs = 60 * 60 * 1000;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: oneHourInMs,
+    },
+  },
+});
+
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Outlet />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/pool/components/PoolsOverview/YourPositions/index.tsx
+++ src/pages/pool/components/PoolsOverview/YourPositions/index.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useMemo, useState } from 'react';
-import { useChains } from 'context/Chains';
-import { ethers } from 'ethers';
-import { Link } from 'react-router-dom';
+import {useEffect, useMemo, useState} from 'react';
+import {ethers} from 'ethers';
+import {Link} from 'react-router-dom';
+import {useQuery} from 'react-query';
+import Skeleton from 'react-loading-skeleton';
+import {useChains} from 'context/Chains';
 import AssetOverview from '../../AssetOverview';
 import lpTokenABI from 'contracts/LPToken.abi.json';
-import { useWalletProvider } from 'context/WalletProvider';
+import {useWalletProvider} from 'context/WalletProvider';
 
 function YourPositions() {
-  const { accounts } = useWalletProvider()!;
-  const { fromChainRpcUrlProvider } = useChains()!;
+  const {accounts} = useWalletProvider()!;
+  const {fromChainRpcUrlProvider} = useChains()!;
   const lpContract = useMemo(() => {
     return new ethers.Contract(
       '0xF9e13773D10C0ec25369CC4C0fAEef05eC00B18b',
@@ -16,18 +18,35 @@ function YourPositions() {
       fromChainRpcUrlProvider,
     );
   }, [fromChainRpcUrlProvider]);
+  const {isLoading, isError, data} = useQuery(
+    'userPositions',
+    getUserPositions,
+    {
+      // Execute only when accounts are available.
+      enabled: !!accounts,
+    },
+  );
   const [userPositions, setUserPositions] = useState(null);
 
-  useEffect(() => {
-    async function getUserPositions(userAddress: string) {
-      const userPositions = await lpContract.getAllNftIdsByUser(userAddress);
-      setUserPositions(userPositions);
-    }
+  function getUserPositions() {
+    console.log(accounts?.[0]);
+    return lpContract
+      .getAllNftIdsByUser(accounts?.[0])
+      .then((res: any) => res.json());
+  }
 
-    if (accounts) {
-      getUserPositions(accounts[0]);
-    }
-  }, [accounts, lpContract]);
+  console.log({isLoading, isError, data});
+
+  // useEffect(() => {
+  //   async function getUserPositions(userAddress: string) {
+  //     const userPositions = await lpContract.getAllNftIdsByUser(userAddress);
+  //     setUserPositions(userPositions);
+  //   }
+
+  //   if (accounts) {
+  //     getUserPositions(accounts[0]);
+  //   }
+  // }, [accounts, lpContract]);
 
   return (
     <article className="mb-2.5 rounded-10 bg-white p-2.5">
@@ -52,42 +71,77 @@ function YourPositions() {
       </header>
 
       <section className="grid grid-cols-2 gap-2.5">
-        <AssetOverview
-          apy={81.19}
-          chainId={1}
-          poolShare={0.02}
-          redirectToManageLiquidity
-          tokenSupplied={59.64}
-          tokenSymbol="ETH"
-          unclaimedFees={651}
-        />
-        <AssetOverview
-          apy={91.91}
-          chainId={43114}
-          poolShare={0.03}
-          redirectToManageLiquidity
-          tokenSupplied={459.64}
-          tokenSymbol="USDC"
-          unclaimedFees={154}
-        />
-        <AssetOverview
-          apy={80.5}
-          chainId={1}
-          poolShare={0.02}
-          redirectToManageLiquidity
-          tokenSupplied={1059.64}
-          tokenSymbol="USDT"
-          unclaimedFees={157}
-        />
-        <AssetOverview
-          apy={71.55}
-          chainId={137}
-          poolShare={0.025}
-          redirectToManageLiquidity
-          tokenSupplied={9999.64}
-          tokenSymbol="BICO"
-          unclaimedFees={1547}
-        />
+        {!isLoading ? (
+          <>
+            <AssetOverview
+              apy={81.19}
+              chainId={1}
+              poolShare={0.02}
+              redirectToManageLiquidity
+              tokenSupplied={59.64}
+              tokenSymbol="ETH"
+              unclaimedFees={651}
+            />
+            <AssetOverview
+              apy={91.91}
+              chainId={43114}
+              poolShare={0.03}
+              redirectToManageLiquidity
+              tokenSupplied={459.64}
+              tokenSymbol="USDC"
+              unclaimedFees={154}
+            />
+            <AssetOverview
+              apy={80.5}
+              chainId={1}
+              poolShare={0.02}
+              redirectToManageLiquidity
+              tokenSupplied={1059.64}
+              tokenSymbol="USDT"
+              unclaimedFees={157}
+            />
+            <AssetOverview
+              apy={71.55}
+              chainId={137}
+              poolShare={0.025}
+              redirectToManageLiquidity
+              tokenSupplied={9999.64}
+              tokenSymbol="BICO"
+              unclaimedFees={1547}
+            />
+          </>
+        ) : (
+          <>
+            <Skeleton
+              baseColor="#615ccd20"
+              enableAnimation
+              highlightColor="#615ccd05"
+              className="!h-37.5 !rounded-7.5"
+              containerClassName="block leading-none"
+            />
+            <Skeleton
+              baseColor="#615ccd20"
+              enableAnimation
+              highlightColor="#615ccd05"
+              className="!h-37.5 !rounded-7.5"
+              containerClassName="block leading-none"
+            />
+            <Skeleton
+              baseColor="#615ccd20"
+              enableAnimation
+              highlightColor="#615ccd05"
+              className="!h-37.5 !rounded-7.5"
+              containerClassName="block leading-none"
+            />
+            <Skeleton
+              baseColor="#615ccd20"
+              enableAnimation
+              highlightColor="#615ccd05"
+              className="!h-37.5 !rounded-7.5"
+              containerClassName="block leading-none"
+            />
+          </>
+        )}
       </section>
     </article>
   );

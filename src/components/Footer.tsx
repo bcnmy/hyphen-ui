72eb59bd9ee347c17f6a00521f8ff04e7c6a437e
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-21 11:03:54 +0530

    Fixed header and footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import poweredByBiconomy from "../assets/images/powered-by-biconomy.svg";
 
 function Footer() {
   return (
-    <footer className="sticky bottom-0 flex w-full items-center justify-center px-6 py-4">
+    <footer className="sticky bottom-0 z-20 flex w-full items-center justify-center bg-black bg-opacity-30 px-6 py-4 backdrop-blur-md">
       <a
         target="_blank"
         href="https://biconomy.io/"

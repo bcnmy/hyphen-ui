a252e77ffe6cc7b8d1ecc834142da87d8798772e
Asheer Rizvi <asheer.vil@gmail.com>
2022-08-29 19:56:00 +0530

    chore: disabled claim reward for avalanche

--- src/pages/farms/components/ManageStakingPosition/index.tsx
+++ src/pages/farms/components/ManageStakingPosition/index.tsx
@@ -357,7 +357,11 @@ function ManageStakingPosition() {
                   {currentChainId === chain?.chainId ? (
                     <button
                       className="mt-10 mb-[3.125rem] flex h-15 w-full items-center justify-center rounded-2.5 bg-hyphen-purple font-semibold text-white disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-hyphen-gray-300 xl:mb-0"
-                      disabled={isDataLoading || unclaimedRewardToken <= 0}
+                      disabled={
+                        isDataLoading ||
+                        unclaimedRewardToken <= 0 ||
+                        (!!chainId && Number.parseInt(chainId) === 43114)
+                      }
                       onClick={handleClaimFeeClick}
                     >
                       {unclaimedRewardToken <= 0 ? (

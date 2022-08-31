8aa135f75e2ec7c81ff3102c7c316d4210afdfe7
Asheer Rizvi <asheer.vil@gmail.com>
2022-08-31 19:10:27 +0530

    feat: removed background opacity

--- src/components/SuperHeader.tsx
+++ src/components/SuperHeader.tsx
@@ -3,7 +3,7 @@ import { HiInformationCircle } from 'react-icons/hi';
 
 function SuperHeader({ superHeaderMsg }: { superHeaderMsg: string }) {
   return (
-    <div className="flex w-full items-center justify-center bg-[#EF940070] px-6 py-2 font-sans text-sm font-semibold text-white">
+    <div className="flex w-full items-center justify-center bg-[#a7731d] px-6 py-2 font-sans text-sm font-semibold text-white">
       <HiInformationCircle className="mr-2 h-3 w-3" />
       {superHeaderMsg}
     </div>

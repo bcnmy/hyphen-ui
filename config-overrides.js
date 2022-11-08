29b79631f0a0c90e9db1a2d2f2befdb4d49baf49
talha <talha.malik@ideofuzion.com>
2022-11-08 17:49:59 +0500

    packages mismatch

--- config-overrides.js
+++ config-overrides.js
@@ -11,6 +11,7 @@ module.exports = function override(config) {
         "os": require.resolve("os-browserify"),
         "https": require.resolve("https-browserify"),
         "url": require.resolve("url"),
+        "zlib": require.resolve("browserify-zlib"),
         "process/browser": require.resolve("process/browser")
     })
     config.resolve.fallback = fallback;
